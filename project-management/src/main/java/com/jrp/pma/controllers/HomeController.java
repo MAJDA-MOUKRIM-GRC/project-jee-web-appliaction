898ba82fb391d229212d7157aa4ecd47a583cb53
TOSHIBA <TOSHIBA@DESKTOP-0UD8BNB>
2022-06-24 21:42:20 +0100

    JEE PROJECT

---
+++ project-management/src/main/java/com/jrp/pma/controllers/HomeController.java
@@ -0,0 +1,54 @@
+package com.jrp.pma.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jrp.pma.dto.ChartData;
+import com.jrp.pma.dto.EmployeeProject;
+import com.jrp.pma.entities.Project;
+import com.jrp.pma.services.EmployeeService;
+import com.jrp.pma.services.ProjectService;
+
+@Controller
+public class HomeController {
+	
+	@Value("${version}")
+	private String ver;
+	
+	@Autowired
+	ProjectService proService;
+	
+	@Autowired
+	EmployeeService empService;
+
+
+	@GetMapping("/")
+	public String displayHome(Model model) throws JsonProcessingException {
+		
+		model.addAttribute("versionNumber", ver);
+		
+		List<Project> projects = proService.getAll();
+		model.addAttribute("projectsList", projects);
+		
+		List<ChartData> projectData = proService.getProjectStatus();
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(projectData);
+		
+		model.addAttribute("projectStatusCnt", jsonString);
+		
+		List<EmployeeProject> employeesProjectCnt = empService.employeeProjects();
+		model.addAttribute("employeesListProjectsCnt", employeesProjectCnt);
+		
+		
+		return "main/home";
+		
+	}
+}
